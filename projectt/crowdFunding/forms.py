ff6fa77dfb4264d10ddb5107d48fa822387b12db
Hanan Mohamed <7ananm7mddd@gmail.com>
2022-02-07 20:04:21 +0200

    Added form design

--- projectt/crowdFunding/forms.py
+++ projectt/crowdFunding/forms.py
@@ -12,7 +12,8 @@ class RegisterationForm(forms.ModelForm):
 
 
 class loginForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
+    Email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control'}))
     class Meta:
         model=myuser
         fields= ['Email','password']
@@ -20,7 +21,4 @@ class loginForm(forms.ModelForm):
 class EditForm(forms.ModelForm):
     class Meta:
         model = myuser
-        #fields= ['first_name','last_name','country','password', 'birthdate','phone_number']
-        #fields = ['first_name','last_name','country','password','phone_number','birthdate','fb_account']
-        #fields='__all__'
         exclude = ('Email','profile_picture','confirm_password')
